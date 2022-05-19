f411debc3ea2e7153ea1e286cffa6fec0db003f4
Pavel Mostovoy <pavelm@bemobi.com>
2022-05-19 13:16:21 +0200

    data update

---
+++ tests/conftest.py
@@ -0,0 +1,38 @@
+def pytest_bdd_before_scenario(request, feature, scenario):
+    """Called before scenario is executed"""
+    pass
+
+
+def pytest_bdd_after_scenario(request, feature, scenario):
+    """Called after scenario is executed (even if one of steps has failed)"""
+    pass
+
+
+def pytest_bdd_before_step(request, feature, scenario, step, step_func):
+    """ - Called before step function is executed and it’s arguments
+    evaluated"""
+    print("before step")
+
+
+def pytest_bdd_before_step_call(request, feature, scenario, step, step_func,
+                                step_func_args):
+    """- Called before step function is executed with evaluated arguments"""
+    print("after step")
+
+
+def pytest_bdd_after_step(request, feature, scenario, step, step_func,
+                          step_func_args):
+    """- Called after step function is successfully executed"""
+    pass
+
+
+def pytest_bdd_step_error(request, feature, scenario, step, step_func,
+                          step_func_args, exception):
+    """ - Called when step function failed to execute"""
+    pass
+
+
+def pytest_bdd_step_func_lookup_error(request, feature, scenario, step,
+                                      exception):
+    """ - Called when step lookup failed"""
+    pass
---
+++ tests/features/web_login.feature
@@ -0,0 +1,7 @@
+@cucumber-basket
+Feature: Web Test
+  As a User I open page,
+
+  Scenario: Open Page
+    Given As BILL
+    Given URL page https://aqa.science
