850ba9cefe3105bc8367952bf9198403ae327f8f
Pavel Mostovoy <pavelm@bemobi.com>
2022-05-26 19:56:54 +0200

    data update

--- api_requests/api_calls.py
+++ api_requests/api_calls.py
@@ -9,15 +9,26 @@ URL = "https://www.aqa.science"
 USER = "admin"
 PASSWORD = "admin123"
 
-
 first_response = requests.get(URL).json()
 
-for item in first_response.values():
-    second_response = requests.get(item, auth=(USER, PASSWORD))
-    with open("response.json", "w") as f:
-        json.dump(second_response.json(), f)
+results = []
 
-    # break for first item
-    break
-    # print(second_response.status_code)
-    # print(second_response.json())
+
+def get_user_list(users_url):
+    global results
+    response = requests.get(users_url, auth=(USER, PASSWORD))
+    initial_object = response.json()
+    results += initial_object["results"]
+    next_ = initial_object["next"]
+    if next_:
+        get_user_list(next_)
+
+
+get_user_list("https://www.aqa.science/users")
+
+print(len(results))
+
+with open("response.json", "w") as f:
+    json.dump(results, f)
+
+
