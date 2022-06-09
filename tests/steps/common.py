155a9ecd8fee31f20b59a47da99280760061577b
Pavel Mostovoy <pavelm@bemobi.com>
2022-06-09 19:40:07 +0200

    data update

--- tests/steps/common.py
+++ tests/steps/common.py
@@ -14,13 +14,14 @@ def first_user(request, name_0):
 @when(parsers.re("Add additional user '(?P<name>.*)'"))
 def second_user(name, name_0, request):
     request.second_name = name
+    request.primary_name = name_0
     print(f"added name {name} {name_0}")
     return name
 
 
 @then("Compare Users names")
 def comparation_l(request):
-    print("final result")
+    assert request.primary_name == request.second_name
     # assert request.login == request.second_name
 
 #
