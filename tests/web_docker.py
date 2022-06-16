29b60f4849d4caa0c5c1c3d7a2a47b8caab8af4b
Pavel Mostovoy <pavelm@bemobi.com>
2022-06-16 19:28:32 +0200

    data update

--- tests/web_docker.py
+++ tests/web_docker.py
@@ -9,12 +9,17 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
 
+port = 4347
 
 try:
-    subprocess.run("docker run -d --name selenium_chrome -p 4347:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome", shell=True,check=True)
+    subprocess.run(f"docker run -d --name selenium_chrome -p"
+                   f" {port}:4444 -v "
+                   f"/dev/shm:/dev/shm selenium/standalone-chrome",
+                   shell=True,check=True)
     time.sleep(5)
     print("docker started")
-    driver = webdriver.Remote(command_executor='http://localhost:4347/wd/hub',
+    driver = webdriver.Remote(
+            command_executor=f'http://localhost:{port}/wd/hub',
                           options=options)
 
     driver.get("https://google.com")
